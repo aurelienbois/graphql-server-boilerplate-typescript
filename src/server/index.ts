3cf647b6d10ae173bbbe27aa4885276277d95ebc
Aurélien Bois <cci@aurelienbois.fr>
2023-11-16 10:06:40 +0100

    type casting as temporary fix for versions conflict

--- src/server/index.ts
+++ src/server/index.ts
@@ -32,7 +32,7 @@ for (const name of Object.keys(services)) {
   if (name === "graphql") {
     ;(async () => {
       await services[name].start()
-      services[name].applyMiddleware({ app })
+      services[name].applyMiddleware({ app: app as any }) // régler le conflit de versions
     })()
   } else {
     app.use(`/$name`, services[name])
